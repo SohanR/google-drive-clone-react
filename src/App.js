3c0d94a523c3d739c85b774c4166bd07206be49a
sohanr <mizan.rahman66d@gmail.com>
2021-02-20 20:59:46 +0600

    import components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import { useState } from 'react';
 import './App.css';
+import FilesView from './components/FilesView/FilesView';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-
+import SideIcon from './components/SideIcon';
+import { auth, provider } from './firebase';
+import GDlogo from './media/google-drive-logo.png';
 function App() {
 
   //auth
 
+  const [user, setUser] = useState();
+
+
+  const handleLogin = ()=>{
+    if(!user){
+      auth.signInWithPopup(provider).then((result) =>{
+        setUser(result.user)
+      })
+    }
+  }
 
   return (
     <div className="App">
-      <Header/>
-      <Sidebar/>
-      {/* auth true
-      sidebar
-      file view
-      sideIcons 
-      */}
-     {/* no auth: log in  */}
+
+      {
+        user ? (
+          <>
+            <Header userPhoto={user.photoURL} />
+
+            <div className="app_main">
+              <Sidebar/>
+              <FilesView/>
+              <SideIcon/>
+            </div>
+          </>
+        ) : (
+          <div className='app_login' >
+            <img src={GDlogo} alt="Google Drive"/>
+            <button onClick={handleLogin} >Log in to Google Drive</button>
+          </div>
+        )
+      }
+     
     </div>
   );
 }
